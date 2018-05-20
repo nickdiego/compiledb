9f491231336f49e192b0e06eb36518974e3e07e3
Nick Diego Yamane <nick@diegoyam.com>
2018-05-20 04:14:46 -0400

    Use __version__.py file to store version info
    - Based on requests' implementation:
      https://github.com/requests/requests
    
    Signed-off-by: Nick Diego Yamane <nick@diegoyam.com>

---
+++ compiledb/__version__.py
@@ -0,0 +1,8 @@
+__title__ = 'compiledb'
+__description__ = 'Tool for generating LLVM Compilation Database files for make-based build systems.'
+__url__ = 'https://github.com/nickdiego/compiledb-generator'
+__version__ = '0.9'
+__author__ = 'Nick Yamane'
+__author_email__ = 'nick@diegoyam.com'
+__license__ = 'GPLv3'
+__copyright__ = 'Copyright 2017 Nick Yamane'
