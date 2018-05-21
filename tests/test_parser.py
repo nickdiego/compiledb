0194d69b1e7f4afc309b9029f82b173916cbe334
Nick Diego Yamane <nick@diegoyam.com>
2018-05-21 00:34:03 -0400

    test: Check trivial build command parsing
    Signed-off-by: Nick Diego Yamane <nick@diegoyam.com>

--- tests/test_parser.py
+++ tests/test_parser.py
@@ -28,10 +28,33 @@ def test_empty():
     exclude_list = []
     verbose = False
 
-    result = parse_build_log(build_log, proj_dir, incpath_prefix, exclude_list, verbose)
+    result = parse_build_log(build_log, proj_dir, incpath_prefix, exclude_list,
+                             verbose)
     assert result.count == 0
     assert result.skipped == 0
     assert result.compdb is not None
     assert type(result.compdb) == list
     assert len(result.compdb) == 0
 
+
+def test_trivial_build_command():
+    pwd = '/build'
+
+    build_log = ['gcc -o hello.o -c hello.c']
+    result = parse_build_log(
+        build_log,
+        proj_dir=pwd,
+        inc_prefix=None,
+        exclude_list=[],
+        verbose=False)
+
+    assert result.count == 1
+    assert result.skipped == 0
+    assert len(result.compdb) == 1
+    assert result.compdb[0] == {
+        'directory': '/build',
+        'file': 'hello.c',
+        'arguments': [
+            'cc', '-c', 'hello.c'
+        ]
+    }
