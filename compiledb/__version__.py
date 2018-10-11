4cdcf9d6607e7196c8a1a202854df63732940d76
Nick Diego Yamane <nick@diegoyam.com>
2018-10-11 15:51:17 -0400

    Bump version to v0.9.8
    Signed-off-by: Nick Diego Yamane <nick@diegoyam.com>

--- compiledb/__version__.py
+++ compiledb/__version__.py
@@ -1,7 +1,7 @@
 __title__ = 'compiledb'
 __description__ = 'Tool for generating Clang JSON Compilation Database files for make-based build systems.'
 __url__ = 'https://github.com/nickdiego/compiledb-generator'
-__version__ = '0.9.7'
+__version__ = '0.9.8'
 __author__ = 'Nick Yamane'
 __author_email__ = 'nick@diegoyam.com'
 __license__ = 'GPLv3'
