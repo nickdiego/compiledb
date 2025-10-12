3e1aa96263e4828e20616eb8b8166efbf2bdf6d7
timmy-quinn <tquinn5301@gmail.com>
2025-10-12 12:42:01 -0400

    removed unnecessary import

--- compiledb/parser.py
+++ compiledb/parser.py
@@ -18,7 +18,6 @@
 #   You should have received a copy of the GNU General Public License
 #   along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
-from sre_compile import IN_LOC_IGNORE
 import bashlex
 import re
 import logging
